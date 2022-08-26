26cba7d55ef7635c2fa7ef947b89c35d4ab2b1d2
Ignacio Rossetto <ignaciorossetto@hotmail.com>
2022-08-26 14:50:13 -0300

    TP1

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const jeje = document.getElementById('hola')
-jeje.innerHTML = 'HOLA men xd'
+let carrito = 0;
+let totalBool = true;
+let producto = 0
+
+
+while (totalBool) {
+  producto = Number(
+    prompt(
+      "Valor del producto ingresado al carrito o -1 cuando haya terminado."
+    )
+  );
+  if (producto === -1) {
+    totalBool = false;
+  } else {
+  carrito += producto;
+  }
+}
+alert(`El total a cobrar es ${carrito}`);
